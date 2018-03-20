cc7e4a21d199963e028b3fbf6a52aaf7fb57ae6a
zy303007 <36958176+zy303007@users.noreply.github.com>
2018-03-20 23:11:41 +0800

    add iview and modify front

--- front/src/components/account.vue
+++ front/src/components/account.vue
@@ -1,3 +1,61 @@
+<style scoped>
+.standard{
+    margin-top:20px;
+    margin-bottom: 20px;
+    margin-left:80px;
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+}
+li,p{
+  margin-top:2px;
+  margin-left:20px;
+  margin-right: 10px;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  font-size: 12px;  
+}
+h3{
+  margin-top: 10px;
+  margin-left: 15px;
+}
+.table{
+  margin-left:20px;
+  margin-top: 10px; 
+}
+.cancel{
+  float: right;
+  margin-right:10px;
+}
+.met-head{
+  font-weight: bold;
+  color: cornflowerblue;
+}
+.information-space{
+  padding: 5px;
+}
+.information-text{
+  margin-left: 30px;
+}
+.distance{
+  margin-top: 15px;
+}
+.icon{
+  opacity: 0.6;
+}
+.icon:hover{
+  opacity: 1.0;
+}
+.cond{
+  width: 50px;
+  heigth:50px;
+  float: right;
+}
+.table{
+  margin: 10px;
+}
+.gray{
+  background: #eee;
+  margin-top: 15px;
+}
+</style>
 <template>
   <div id="account">
     <div v-show="infoEdit">
@@ -67,83 +125,50 @@
       </div>
     </div>
     <div v-show="!infoEdit && !passEdit">
-      <div class="weui-cells__title">部门信息</div>
-      <div class="weui-cells weui-cells_form">
-        <div class="weui-cell">
-          <div class="weui-cell__hd">
-            <label class="weui-label">部门</label>
+      <div> 
+        <h3>部门信息
+            <Button class="cancel" type="error" size="small"  @click="logout">注销</Button>
+        </h3>
+        <card class="distance" dis-hover :bordered="false"> 
+          <div class="information-space"><label>部门</label><label class="information-text">{{user.account}}</label></div>
+          <div class="information-space"><label>部长</label><label class="information-text">{{user.name ? user.name : '请完善信息'}}</label></div>
+          <div class="information-space"><label>手机</label><label class="information-text">{{user.phone ? user.phone : '请完善信息'}}</label></div>
+          <div class="information-space"><label>钱包</label><label class="information-text">{{user.wallet + '￥'}}</label></div>
+          <div>
+            <Tooltip content="编辑信息"><Button type="primary" shape="circle" icon="edit" @click="clickInfoEdit"></Button></Tooltip>
+            <Tooltip content="修改密码"><Button type="primary" shape="circle" icon="ios-gear" @click="clickPassEdit"></Button></Tooltip>
           </div>
-          <div class="weui-cell__bd">{{ user.account }}</div>
-        </div>
-        <div class="weui-cell">
-          <div class="weui-cell__hd">
-            <label class="weui-label">部长</label>
-          </div>
-          <div class="weui-cell__bd">{{ user.name ? user.name : '请完善信息' }}</div>
-        </div>
-        <div class="weui-cell">
-          <div class="weui-cell__hd">
-            <label class="weui-label">手机</label>
-          </div>
-          <div class="weui-cell__bd">{{ user.phone ? user.phone : '请完善信息' }}</div>
-        </div>
-        <div class="weui-cell">
-          <div class="weui-cell__hd">
-            <label class="weui-label">钱包</label>
-          </div>
-          <div class="weui-cell__bd">{{ '￥' + user.wallet }}</div>
-        </div>
+        </card>
+        <card class="gray" >
+          <Tabs size="small">
+            <TabPane label="物资借用">
+              <card v-for="(matbook,index) of matbooks" class="distance">
+                  <p slot="title">
+                    {{matbook.activity}}
+                    <Button v-show="matbook.cond === 0" class="cancel" type="error" size="small"  @click="cancelMatbook(matbook._id)">撤销</Button>
+                  </p>
+                  <p>{{matbook.name}} <a :href="'tel:' + matbook.phone">{{matbook.phone}}</a>
+                  <img v-show="matbook.cond === 0" class="cond" src="../assets/img/book.png">
+                  <img v-show="matbook.cond === 1" class="cond" src="../assets/img/lent.png"></p>
+                  <p>{{ matbook.takeDate }}借用</p>
+                  <p>{{ matbook.returnDate }}将归还</p>
+                  <Table class="table" size="small" stripe :columns="columns1" :data="data1[index]"></Table>
+                  <Button v-show="matbook.cond !== 1" size="small" @click="lentMatbook(matbook._id)">借出</Button>
+                  <Button size="small" disabled>转交</Button>
+              </card>
+            </TabPane>
+            <TabPane label="会议预约">
+              <card v-for="metbook in metbooks">
+                  <span class="met-head">会议：{{ metbook.activity }}</span><Button class="cancel"  size="small" icon="log-out" shape="circle" @click="cancelMetbook(metbook._id)"></Button><br>
+                  预约人：{{ metbook.name }} ({{ metbook.phone }})<br>
+                  预约日期：{{ metbook.date }}<br>
+                  预约时间：{{ metbook.time }}<br>
+              </card>
+            </TabPane>
+          </Tabs> 
+        </card>
       </div>
-      <div class="weui-cells__title">物资申请</div>
-      <div class="weui-cells">
-        <a class="weui-cell weui-cell_access" v-for="matbook in matbooks" @click="failMatbook(matbook._id)">
-          <div class="weui-cell__bd">
-            活动：{{ matbook.activity }}<br>
-            预约人：{{ matbook.name }} ({{ matbook.phone }})<br>
-            领取时间：{{ matbook.takeDate }}<br>
-            归还时间：{{ matbook.returnDate }}<br>
-            借用物资：<label v-for="(material, index) of matbook.materials"><b>({{ index + 1 }})&nbsp{{ material.material.name }} × {{ material.book }}&nbsp&nbsp</b></label><br>
-            目前状态：{{ cond[matbook.cond] }}
-          </div>
-          <div class="weui-cell__ft"></div>
-        </a>
-        <div class="weui-cell" v-show="!matbooks.length">
-          <div class="weui-cell__bd">暂无物资申请记录</div>
-        </div>
-      </div>
-      <div class="weui-cells__title">会议室预约</div>
-      <div class="weui-cells">
-        <a class="weui-cell weui-cell_access" v-for="metbook in metbooks" @click="failMetbook(metbook._id)">
-          <div class="weui-cell__bd">
-            会议：{{ metbook.activity }}<br>
-            预约人：{{ metbook.name }} ({{ metbook.phone }})<br>
-            预约日期：{{ metbook.date }}<br>
-            预约时间：{{ metbook.time }}
-          </div>
-          <div class="weui-cell__ft"></div>
-        </a>
-        <div class="weui-cell" v-show="!metbooks.length">
-          <div class="weui-cell__bd">暂无会议室预约记录</div>
-        </div>
-      </div>
-      <div class="weui-cells__title">部门账号设置</div>
-      <div class="weui-cells">
-        <a class="weui-cell weui-cell_access" @click="clickInfoEdit">
-          <div class="weui-cell__hd"><label class="weui-label">信息更改</label></div>
-          <div class="weui-cell__bd"></div>
-          <div class="weui-cell__ft"></div>
-        </a>
-        <a class="weui-cell weui-cell_access" @click="clickPassEdit">
-          <div class="weui-cell__hd"><label class="weui-label">密码更改</label></div>
-          <div class="weui-cell__bd"></div>
-          <div class="weui-cell__ft"></div>
-        </a>
-      </div>
-      <div class="weui-msg__opr-area">
-        <p class="weui-btn-area">
-          <a @click="logout" class="weui-btn weui-btn_warn">注销</a>
-        </p>
-      </div>
+      
     </div>
   </div>
 </template>
@@ -168,7 +193,24 @@ export default {
       passEdit: false,
       matbooks: [],
       metbooks: [],
-      cond: [ '预约', '借出', '归还', '作废' ]
+      cond: [ '预约', '借出', '归还', '作废' ],
+      columns1: [
+        {
+          title: '名称',
+          key: 'name'
+        },
+        {
+          title: '数量',
+          key: 'book'
+        },
+        {
+          title: '单位',
+          key: 'material'
+        }
+      ],
+      data1: [],
+      data2: this.getMockData(),
+      targetKeys1: this.getTargetKeys()
     }
   },
   activated () {
@@ -231,26 +273,59 @@ export default {
     getMatbooks () {
       api.handleApi(api.getMatbooks({ user: this.userId }), res => {
         this.matbooks = res.body.list
+        this.matbooks.forEach((value, i) => {
+          this.data1[i] = []
+          for (let j = 0; j < value.materials.length; ++j) {
+            this.data1[i].push({
+              key: (j + 1).toString(),
+              name: value.materials[j].material.name,
+              book: value.materials[j].book
+            })
+          }
+        })
       }, '正在获取信息')
     },
-    failMatbook (_id) {
-      weui.actionSheet([{
-        label: '撤销申请',
-        onClick: () => {
-          api.handleApi(api.putMatbooks({ _id, cond: 3 }), res => {
-            weui.toast('撤销成功', 1500)
+    lentMatbook (_id) {
+      this.$Modal.confirm({
+        content: '<p>是否验收无误，并确认借出物资？</p>',
+        onOk: () => {
+          api.handleApi(api.putMatbooks({
+            _id, cond: 1}), res => {
+            this.$Message.info('已借出')
             this.getMatbooks()
-          }, '正在撤销')
-        }
-      }], [{
-        label: '取消',
-        onClick: () => {}
-      }])
+          }, '正在借出')
+        },
+        onCancel: () => {}
+      })
     },
     getMetbooks () {
       api.handleApi(api.getMetbooks({ user: this.userId }), res => {
         this.metbooks = res.body.list
       }, '正在获取信息')
+    },
+    cancelMatbook (_id) {
+      this.$Modal.confirm({
+        content: '<p>是否撤销该项申请？</p>',
+        onOk: () => {
+          api.handleApi(api.putMatbooks({ _id, cond: 3 }), res => {
+            this.$Message.info('撤销成功')
+            this.getMatbooks()
+          }, '正在撤销')
+        },
+        onCancel: () => {}
+      })
+    },
+    cancelMetbook (_id) {
+      this.$Modal.confirm({
+        content: '<p>是否取消会议预约？</p>',
+        onOk: () => {
+          api.handleApi(api.putMetbooks({ _id, cond: 3 }), res => {
+            weui.toast('取消成功', 1500)
+            this.getMetbooks()
+          }, '正在取消')
+        },
+        onCancel: () => {}
+      })
     },
     failMetbook (_id) {
       weui.actionSheet([{
@@ -265,6 +340,33 @@ export default {
         label: '取消',
         onClick: () => {}
       }])
+    },
+    getMockData () {
+      let mockData = []
+      for (let i = 1; i <= 5; i++) {
+        mockData.push({
+          key: i.toString(),
+          name: 'Content ' + i,
+          description: 'The desc of content  ' + i
+        })
+      }
+      return mockData
+    },
+    getTargetKeys () {},
+    render1 (item) {
+      return item.name
+    },
+    handleChange1 (newTargetKeys, direction, moveKeys) {
+      console.log(newTargetKeys)
+      console.log(direction)
+      console.log(moveKeys)
+      this.targetKeys1 = newTargetKeys
+    },
+    ok () {
+      this.$Message.info('Clicked ok')
+    },
+    cancel () {
+      this.$Message.info('Clicked cancel')
     }
   }
 }
--- front/src/main.js
+++ front/src/main.js
@@ -5,8 +5,29 @@ import App from './App'
 import router from './router'
 import store from './store'
 import 'weui'
+import {
+   Button, Form, FormItem, Input, Row, Col, Cascader, Card, DatePicker, Tabs, TabPane, Tooltip, Modal, Message, Icon, Table} from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+Vue.component('Button', Button)
+Vue.component('Form', Form)
+Vue.component('FormItem', FormItem)
+Vue.component('Input', Input)
+Vue.component('Row', Row)
+Vue.component('Col', Col)
+Vue.component('Cascader', Cascader)
+Vue.component('Card', Card)
+Vue.component('DatePicker', DatePicker)
+Vue.component('Tabs', Tabs)
+Vue.component('TabPane', TabPane)
+Vue.component('Tooltip', Tooltip)
+Vue.component('Modal', Modal)
+Vue.component('Message', Message)
+Vue.component('Icon', Icon)
+Vue.component('Table', Table)
+Vue.prototype.$Modal = Modal
+Vue.prototype.$Message = Message
 
 /* eslint-disable no-new */
 new Vue({
